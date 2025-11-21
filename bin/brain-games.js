a3bc28f2ff1d29f1f48d9f15fc368731a0e30b01
Anton Ryabtsev <anton_ryabcev@mail.ru>
2025-11-21 15:20:43 +0300

    Done BG-1.5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import getName from "./src/cli.js";
+import getName from "./src/cli";
+
+const name = getName();
 
 const textCmd = (text) => {
   console.log(text);
 };
 
 textCmd("Welcome to the Brain Games!");
-
-const name = getName();
 
 textCmd(`Hello, ${name}!`);
